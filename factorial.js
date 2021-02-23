1ed0f3b2b85ca6846946c740354486ba421a0bf8
Meg Shulmister <megshulmister@Megs-MacBook-Pro.local>
2021-02-23 16:50:57 -0700

    Solved factorial recursively

---
+++ factorial.js
@@ -0,0 +1,12 @@
+const factorial = (num) => {
+  
+    //returns 4*3*2*1
+    //base case
+  if (num === 1) {
+      return 1;
+  } else {
+      return num * factorial(num - 1);
+  }
+}
+
+console.log(factorial(5))
